7a91125f4eeaa2a8cd5d66341acf506e11c1a605
Andrew <atklenner@gmail.com>
2022-03-29 17:39:57 -0700

    Finish sort sketch

--- js/sorting/sketch.js
+++ js/sorting/sketch.js
@@ -1,10 +1,12 @@
 let array = [];
+let cycles = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   array = new Array(width);
   for (let i = 0; i < width; i++) {
-    array[i] = random(height);
+    // array[i] = random(height);
+    array[i] = noise(i / 100.0) * height;
   }
 }
 
@@ -14,4 +16,16 @@ function draw() {
     stroke(255);
     line(index, height, index, height - value);
   });
+  if (cycles < array.length) {
+    for (let i = 0; i < width - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let val = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = val;
+      }
+    }
+  } else {
+    noLoop();
+  }
+  cycles++;
 }
